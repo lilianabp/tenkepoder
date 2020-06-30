64605f0f93c127ea4758ef2831d06478049b8161
Liliana <liliana@gmail.com>
2020-06-30 19:18:52 +0200

    Add new design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/{_locale}", name="home")
      */
     public function index()
     {
